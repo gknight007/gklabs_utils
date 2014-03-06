82ed0378618a45b76fab70adc188679247d0a6a3
Gregory Knight <gregory.knight@vocalocity.com>
2014-03-06 10:17:21 -0500

    adding sh_lib/util.sh

---
+++ sh_lib/util.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash4
+
+function get_uuid {
+    echo $(cat /proc/sys/kernel/random/uuid 2>/dev/null || uuidgen);
+}
+
+
+
+function is_pid_running {
+    pid="$1"
+    found_pid=$(ps --no-headers $pid)
+    [ -z "$found_pid" ] && return 1
+    return 0
+}
+
+
+
