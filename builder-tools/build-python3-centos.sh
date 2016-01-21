7899cde5082873dc319b15081768f1ec96ca15b8
Gregory Knight <gregory.knight@vocalocity.com>
2016-01-21 15:32:42 -0500

    adding rpm build

--- builder-tools/build-python3-centos.sh
+++ builder-tools/build-python3-centos.sh
@@ -8,10 +8,14 @@ tgzName=$(basename $url)
 pyName=${tgzName/.tgz/}
 startPwd=$PWD
 md5='e80a0c1c71763ff6b5a81f8cc9bb3d50'
+reqPkgList='sqlite-devel openssl-devel rubygems ruby-devel rpm-build'
+pyVer=$(basename $(dirname $url))
 
 sudo yum -y groupinstall 'Development Tools' || die
 
-sudo yum install -y sqlite-devel || die
+sudo yum install -y $reqPkgList || die
+
+sudo gem install fpm || die
 
 mkdir python-build python-prefix
 
@@ -29,5 +33,20 @@ export CFLAGS='-fPIC'
 
 make || die
 
-make install 
+make install  || die
+
+cd $startPwd || die
+
+fpm \
+  -C $startPwd/python-prefix \
+  -t rpm \
+  -s dir \
+  --prefix /usr \
+  --name python3 \
+  --version $pyVer \
+  --license Python \
+  --vendor Python \
+  --url 'http://www.python.org' \
+  -x '*.pyc' \
+  
 
