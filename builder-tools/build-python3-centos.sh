700be485f9e385a8ec80129c677ce125c5b24fb6
Gregory Knight <gregory.knight@vocalocity.com>
2016-01-25 12:35:57 -0500

    case build py3

--- builder-tools/build-python3-centos.sh
+++ builder-tools/build-python3-centos.sh
@@ -11,44 +11,62 @@ md5='e80a0c1c71763ff6b5a81f8cc9bb3d50'
 reqPkgList='sqlite-devel openssl-devel rubygems ruby-devel rpm-build'
 pyVer=$(basename $(dirname $url))
 
-sudo yum -y groupinstall 'Development Tools' || die
 
-sudo yum install -y $reqPkgList || die
+getPkgs () {
+  sudo yum -y groupinstall 'Development Tools' || die
+  sudo yum install -y $reqPkgList || die
+  sudo gem install fpm || die
+}
 
-sudo gem install fpm || die
+mkDirs () {  mkdir python-build python-prefix ; }
 
-mkdir python-build python-prefix
+goToSrcDir () {
+  cd python-build || die
+  cd $pyName || die
+}
 
-cd python-build || die
 
-[ -e "$tgzName" ] || ( wget $url || die )
-#FIXME: add check for MD5
+buildit () {
+  cd python-build || die
 
-tar -zxf $(basename $url) || die
+  [ -e "$tgzName" ] || ( wget $url || die )
+  #FIXME: add check for MD5
 
-cd $pyName || die
+  tar -zxf $(basename $url) || die
 
-export CFLAGS='-fPIC'
-./configure --prefix=${startPwd}/python-prefix || die 
+  cd $pyName || die
 
-make || die
+  export CFLAGS='-fPIC'
+  ./configure --prefix=${startPwd}/python-prefix || die 
 
-make install  || die
+  make || die
+}
 
-cd $startPwd || die
 
-fpm \
-  -C $startPwd/python-prefix \
-  -t rpm \
-  -s dir \
-  --prefix /usr \
-  --name python3 \
-  --version $pyVer \
-  --license Python \
-  --vendor Python \
-  --url 'http://www.python.org' \
-  --provides 'python(abi)' \
-  --provides 'python' \
-  -x '*.pyc' \
-  
+mkRpm () {
+  fpm \
+    -C $startPwd/python-prefix \
+    -t rpm \
+    -s dir \
+    --prefix /usr \
+    --name python3 \
+    --version $pyVer \
+    --license Python \
+    --vendor Python \
+    --url 'http://www.python.org' \
+    --provides 'python(abi)' \
+    --provides 'python' \
+    -x '*.pyc' \
+} 
 
+
+case "$1" in 
+  rpm)
+   ;;
+  build)
+  ;;
+  pkg)
+  ;;
+  *)
+  ;;
+esac
