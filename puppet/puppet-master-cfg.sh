c48a77684c2ac69a5abce872cee17a75f832178e
Gregory Knight <gregory.knight@vocalocity.com>
2016-03-23 16:25:45 -0400

    puppet config template script

---
+++ puppet/puppet-master-cfg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+cat <<EOF
+[master]
+vardir = /opt/puppetlabs/server/data/puppetserver
+logdir = /var/log/puppetlabs/puppetserver
+rundir = /var/run/puppetlabs/puppetserver
+pidfile = /var/run/puppetlabs/puppetserver/puppetserver.pid
+codedir = /etc/puppetlabs/code
+autosign = True
+
+[agent]
+server = $1
+ca_server = $2
+
+[main]
+dns_alt_names = $3
+EOF
+
